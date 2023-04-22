e643540fbe75754eb6c611933694de2e37cfe8d4
Serhii Revva <besha2vox@gmail.com>
2023-04-22 21:38:34 +0300

    qwe

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import { lazy, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getCurrentUser } from 'redux/auth/operations';
+import { lazy } from 'react';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
 import NoFoundPage from 'components/NoFoundPage/NotFoundPage';
 
